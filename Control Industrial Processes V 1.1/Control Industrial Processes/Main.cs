c3cf65f221b7cfc40dce6b19dfe4e5786eb880a2
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2023-01-29 03:27:41 +0330

    Design App

---
+++ Control Industrial Processes V 1.1/Control Industrial Processes/Main.cs
@@ -0,0 +1,149 @@
+﻿//Navid-Derakhshandeh
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System.Diagnostics;
+namespace Control_Industrial_Processes
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            new ToastContentBuilder()
+            .AddText("Welcome")
+            .AddText("Program Has Started")
+            .Show();
+        }
+        private void tempToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Temp frm = new Temp();
+            //frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void tileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void eventToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Event frm = new Event();
+            //frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void valvesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Valves frm = new Valves();
+            //frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void boilerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Boiler frm = new Boiler();
+            //frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void dieselGeneratorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DieselGen frm = new DieselGen();
+            //frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void iDSLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Interpreter frm = new Interpreter();
+            //frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Event frm = new Event();
+            frm.Show();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\Test4\Boiler Efficiency\Boiler Efficiency\bin\Debug\Boiler Efficiency.exe");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            SendData frm = new SendData();
+            frm.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Receiver frm = new Receiver();
+            frm.Show();
+        }
+
+        private void engineerRoomsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ICS3 frm = new ICS3();
+            frm.Show();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            QPG frm = new QPG();
+            frm.Show();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            Debriefing frm = new Debriefing();
+            frm.Show();
+        }
+
+        private void descriptionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DescriptionsAboutCIP frm = new DescriptionsAboutCIP();
+            frm.Show();
+        }
+
+        private void tunnelTurbineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TunnelTurbine1 frm = new TunnelTurbine1();
+            frm.Show();
+        }
+
+        private void designDrawToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DesignApp frm = new DesignApp();
+            frm.Show();
+        }
+    }
+}
---
+++ Control Industrial Processes V 1.1/Control Industrial Processes/SMTER.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Control_Industrial_Processes
+{
+    public partial class SMTER : Form
+    {
+        Socket socket;
+        EndPoint Send, Receive;
+        byte[] Buffer;
+        public SMTER()
+        {
+            InitializeComponent();
+        }
+        private void SMTER_Load(object sender, EventArgs e)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            txtSIP.Text = GetIP();
+            txtDIP.Text = GetIP();
+        }
+        private string GetIP()
+        {
+            IPHostEntry Host;
+            Host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in Host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+        private void MessageReturn(IAsyncResult Res)
+        {
+            try
+            {
+
+
+                byte[] ReceiveddData = new byte[1500];
+                ReceiveddData = (byte[])Res.AsyncState;
+                ASCIIEncoding Encoding = new ASCIIEncoding();
+                string ReceivedMessage = Encoding.GetString(ReceiveddData);
+                LM1.Items.Add("Destination: " + ReceivedMessage);
+
+                Buffer = new byte[1500];
+                socket.BeginReceiveFrom(Buffer, 0, Buffer.Length, SocketFlags.None, ref Send, new AsyncCallback(MessageReturn), Buffer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                Send = new IPEndPoint(IPAddress.Parse(txtSIP.Text), Convert.ToInt32(txtSPort.Text));
+                socket.Bind(Send);
+
+                Receive = new IPEndPoint(IPAddress.Parse(txtDIP.Text),
+                Convert.ToInt32(txtDPort.Text));
+                socket.Connect(Receive);
+
+                Buffer = new byte[1500];
+                socket.BeginReceiveFrom(Buffer, 0, Buffer.Length, SocketFlags.None, ref Receive, new
+                AsyncCallback(MessageReturn), Buffer);
+
+                btnSend.Enabled = true;
+                btnConnect.Text = "Connected";
+                btnConnect.Enabled = false;
+                txtM.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+
+                msg = enc.GetBytes(txtM.Text);
+
+                socket.Send(msg);
+
+
+                LM1.Items.Add("Source: " + txtM.Text);
+
+                txtM.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+    }
+}
