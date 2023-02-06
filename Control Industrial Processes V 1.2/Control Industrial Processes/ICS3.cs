1081797c99f45221c203f594c6e47c7a6b551fc8
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2023-02-06 03:43:07 +0330

    WorkBench ToolKit

---
+++ Control Industrial Processes V 1.2/Control Industrial Processes/ICS3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_Industrial_Processes
+{
+    public partial class ICS3 : Form
+    {
+        public ICS3()
+        {
+            InitializeComponent();
+        }
+
+        private void ICS3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            SMTER frm = new SMTER();
+            frm.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            SD1 frm = new SD1();
+            frm.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            RD3 frm = new RD3();
+            frm.Show();
+        }
+    }
+}
---
+++ Control Industrial Processes V 1.2/Control Industrial Processes/WarningSound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_Industrial_Processes
+{
+    public partial class WarningSound : Form
+    {
+        public WarningSound()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WarningSound_Load(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"D:\Air-raid-siren (online-audio-converter.com).wav");
+            player.Play();
+        }
+
+        
+    }
+}
