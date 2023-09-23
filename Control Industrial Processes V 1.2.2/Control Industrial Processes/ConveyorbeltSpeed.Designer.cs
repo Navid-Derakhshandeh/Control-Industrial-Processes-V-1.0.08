ce59e9752412af156465c9644487ef0173b4639a
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2023-09-23 23:28:41 +0330

    Motion Graphics

---
+++ Control Industrial Processes V 1.2.2/Control Industrial Processes/ConveyorbeltSpeed.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Control_Industrial_Processes
+{
+    partial class ConveyorbeltSpeed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtSpeed = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.ServerIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnC
+            // 
+            this.btnC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.btnC.Location = new System.Drawing.Point(12, 12);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(103, 42);
+            this.btnC.TabIndex = 0;
+            this.btnC.Text = "Connect";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.btnD.Location = new System.Drawing.Point(136, 12);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(111, 42);
+            this.btnD.TabIndex = 1;
+            this.btnD.Text = "Disconnect";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.lbl1.Location = new System.Drawing.Point(253, 23);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(67, 22);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Speed:";
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.txtSpeed.Location = new System.Drawing.Point(326, 23);
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(100, 27);
+            this.txtSpeed.TabIndex = 3;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.txtDate.Location = new System.Drawing.Point(555, 23);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(233, 27);
+            this.txtDate.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label1.Location = new System.Drawing.Point(451, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Date/Time:";
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(12, 133);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(776, 305);
+            this.picBox1.TabIndex = 6;
+            this.picBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label2.Location = new System.Drawing.Point(491, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "C2";
+            // 
+            // ServerIP
+            // 
+            this.ServerIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.ServerIP.Location = new System.Drawing.Point(178, 71);
+            this.ServerIP.Name = "ServerIP";
+            this.ServerIP.Size = new System.Drawing.Size(184, 27);
+            this.ServerIP.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 22);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Server IP Address:";
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.btnBegin.Location = new System.Drawing.Point(388, 61);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(81, 36);
+            this.btnBegin.TabIndex = 10;
+            this.btnBegin.Text = "Begin";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // ConveyorbeltSpeed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ServerIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Name = "ConveyorbeltSpeed";
+            this.Text = "ConveyorbeltSpeed";
+            this.Load += new System.EventHandler(this.ConveyorbeltSpeed_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtSpeed;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ServerIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ Control Industrial Processes V 1.2.2/Control Industrial Processes/WorkBenchToolKit.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Opc.UaFx.Client;
+
+namespace Control_Industrial_Processes
+{
+    public partial class WorkBenchToolKit : Form
+    {
+        OpcClient Opc = new OpcClient("opc.tcp://localhost:62640/");
+        public WorkBenchToolKit()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var Tag1 = "ns=2;s=Tag1";
+            var t1 = Opc.ReadNode(Tag1);
+            var Tag2 = "ns=2;s=Tag2";
+            var t2 = Opc.ReadNode(Tag2);
+            var Tag3 = "ns=2;s=Tag3";
+            var t3 = Opc.ReadNode(Tag3);
+            txtBox4.Text = t1.ToString();
+            txtBox5.Text = t2.ToString();
+            txtBox6.Text = t3.ToString();
+
+            int a = Int32.Parse(txtBox4.Text);
+            int b = Int32.Parse(txtBox5.Text);
+            int c = Int32.Parse(txtBox6.Text);
+
+            moveGauge1.Value = a;
+            moveGauge2.Value = b;
+            moveGauge3.Value = c;
+
+
+
+
+        }
+
+        private void moveButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Opc.Connect();
+                timer1.Start();
+                lbl1.Text = "Connected";
+
+            }
+            catch (Exception ex)
+            {
+                lbl1.Text = ex.ToString();
+                throw;
+            }
+        }
+
+        private void moveButton2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (Opc != null)
+            {
+                Opc.Disconnect();
+            }
+            lbl1.Text = "Disconnected";
+        }
+    }
+}
